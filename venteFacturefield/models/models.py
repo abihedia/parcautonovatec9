557bd25d68b9c3ade289031e7da1d90c88847003
abihedia <51483423+abihedia@users.noreply.github.com>
2022-08-15 12:17:49 +0100

    Add files via upload

---
+++ venteFacturefield/models/models.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api
+
+
+class SaleMoveHeritfacturee(models.Model):
+    _inherit = 'sale.order'
+    sale_date_Facture = fields.Date("Date de prochaine facturation")
+    sale_park = fields.Boolean(default=False)
+    sale_periode = fields.Integer(default='0')
+
+
+    #bon de commande automatique
+
+    sale_date_bon_de_commande = fields.Date("Date de prochaine bon de commande")
+    sale_not_update_bon_commande = fields.Boolean(default=False, string='non à jour')
+    sale_date_de_fin_contrat = fields.Date("Date de fin de contart")
+class AcountMoveHeritFIN(models.Model):
+    _inherit = 'account.move'
+    acount_maintnance = fields.Boolean(string="Maintenance", default=False)
